50dae3bad42b6ae0b38f055d25a0b9d4e3552730
Facundo Brunetti <facundobrunetti99@gmail.com>
2024-01-23 21:12:45 -0300

    firstcommit

---
+++ src/test/java/com/javaproject/javacoder/JavacoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.javacoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
